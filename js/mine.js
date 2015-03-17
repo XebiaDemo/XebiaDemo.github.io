7eb6587b2b1440467ff4bcadffc6e0a422b61f72
Nikhil Nanjappa <nikhilnanjappa@Apples-MacBook-Pro.local>
2015-03-17 12:11:55 +0530

    see2

--- js/mine.js
+++ js/mine.js
@@ -12,3 +12,15 @@ $(function(){
 		$('.reg').css("background","rgba(225,225,225,0)");
 	});
 });
+
+$('.alert').delay(2000).fadeIn(300);
+
+$('.fa-times-circle').click(function(){
+   $(this).parent('.alert').fadeOut(300);
+});
+
+$('.switch').click(function(){
+   $(this).children('i').toggleClass('fa-pencil');
+   $('.login').animate({height: "toggle", opacity: "toggle"}, "slow");
+   $('.register').animate({height: "toggle", opacity: "toggle"}, "slow");
+});
